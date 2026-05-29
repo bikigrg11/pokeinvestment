9156b193e4d7751d098ef2eb4279d0382535ca84
Biki Gurung <bikigurung8@gmail.com>
2026-05-29 10:36:47 -0400

    feat: add creators tRPC router (list/byId/submit/vote/logCall + admin)

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -4,6 +4,7 @@ import { setsRouter } from "./sets";
 import { analyticsRouter } from "./analytics";
 import { pricesRouter } from "./prices";
 import { sealedRouter } from "./sealed";
+import { creatorsRouter } from "./creators";
 
 export const appRouter = createTRPCRouter({
   cards: cardsRouter,
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   prices: pricesRouter,
   analytics: analyticsRouter,
   sealed: sealedRouter,
+  creators: creatorsRouter,
 });
 
 export type AppRouter = typeof appRouter;
